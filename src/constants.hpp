2e1f3117a161b548d5e9fe6583ba38f8a430e9ee
Samik Pradhan <samikpradhan@gmail.com>
2026-04-27 20:59:29 -0700

    creating intial files for sphere

---
+++ src/constants.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+inline int WIN_HEIGHT = 720;
+inline int WIN_WIDTH = 1280;
---
+++ src/sphere.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+/**
+ * Variables
+ * 
+ * position
+ * size (radius)
+ * 
+ * opt: color
+ */
+
+class Sphere {
+private:
+    
+    double radius;
+
+public:
+
+
+}
